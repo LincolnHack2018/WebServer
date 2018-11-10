35fd35adb6d1daf96c336e88a3b8cc2e1e7162fb
cshep4 <chris_shepherd2@hotmail.com>
2018-11-10 14:55:22 +0000

    Added test message endpoint

--- src/main/java/com/hack/collective/hockey/controller/HockeyController.java
+++ src/main/java/com/hack/collective/hockey/controller/HockeyController.java
@@ -1,17 +1,29 @@
 package com.hack.collective.hockey.controller;
 
+import com.hack.collective.hockey.domain.Coordinate;
 import com.hack.collective.hockey.domain.Device;
 import com.hack.collective.hockey.service.HockeyService;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 
 @RestController
 public class HockeyController {
     @Autowired
     private HockeyService hockeyService;
+
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @PostMapping("/init")
     public ResponseEntity initialiseDevice(@RequestBody Device device) {
@@ -22,6 +34,17 @@ public class HockeyController {
     @PostMapping("/teardown")
     public ResponseEntity teardown() {
         hockeyService.tearDown();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity testMessage() {
+        val c = Coordinate.builder()
+                .x(1.0f)
+                .y(2.0f)
+                .build();
+
+        template.convertAndSend("/initialise", Optional.of(singletonList(c)));
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/hack/collective/hockey/domain/Coordinate.java
+++ src/main/java/com/hack/collective/hockey/domain/Coordinate.java
@@ -1,8 +1,10 @@
 package com.hack.collective.hockey.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Coordinate {
     private float x;
     private float y;

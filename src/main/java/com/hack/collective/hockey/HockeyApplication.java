1095e879468f2d304df9cd307a6439431adb93ea
cshep4 <chris_shepherd2@hotmail.com>
2018-11-10 12:57:29 +0000

    Initial commit

---
+++ src/main/java/com/hack/collective/hockey/HockeyApplication.java
@@ -0,0 +1,12 @@
+package com.hack.collective.hockey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeyApplication.class, args);
+	}
+}

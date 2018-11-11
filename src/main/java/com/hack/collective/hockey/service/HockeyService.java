8f7d0803fd208ebb0d2894636825e6e36ce4f844
cshep4 <chris_shepherd2@hotmail.com>
2018-11-11 02:15:54 +0000

    websocket

--- src/main/java/com/hack/collective/hockey/service/HockeyService.java
+++ src/main/java/com/hack/collective/hockey/service/HockeyService.java
@@ -41,7 +41,7 @@ public class HockeyService {
         timer = System.currentTimeMillis();
 
         while (true) {
-            if (System.currentTimeMillis() - timer > 1000) {
+            if (System.currentTimeMillis() - timer > 1500) {
                 System.out.println(devices);
                 if (devices.size() <= 1) {
                     sendData(emptyList());
